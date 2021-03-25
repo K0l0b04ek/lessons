8fe662f2bc0f2e9401bd059dd3f74cdfa4add278
K0l0b04ek <74917161+K0l0b04ek@users.noreply.github.com>
2021-03-25 10:19:03 +0400

    Add files via upload

---
+++ Новая папка/data/news.py
@@ -0,0 +1,30 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+categories = orm.relation("Category",
+                          secondary="association",
+                          backref="news")
+
+
+class News(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'news'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+
+    def __repr__(self):
+        return (f"<News> {self.user.name} {self.user_id} {self.title} {self.content} {self.created_date}")
